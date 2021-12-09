7ef94a19fc5d42b2094cf06a6e05340cd96335ec
Matthew Laufer <matthew.j.laufer@gmail.com>
2021-12-08 21:58:51 -0800

    vim: create bash script for starting chrome debugger

--- nvim/.config/nvim/plugin/debugger.lua
+++ nvim/.config/nvim/plugin/debugger.lua
@@ -1,10 +1,9 @@
+local dap = require('dap')
 local dap_install = require('dap-install')
+
 dap_install.config('chrome', {})
-dap_install.config('jsnode', {})
 
-local dap = require('dap');
-
-dap.configurations.javascriptreact = { -- change to javascript if needed
+esconfig = {
     {
         type = 'chrome',
         request = 'attach',
@@ -13,21 +12,20 @@ dap.configurations.javascriptreact = { -- change to javascript if needed
         sourceMaps = true,
         protocol = 'inspector',
         port = 9222,
-        webRoot = '${workspaceFolder}'
+        webRoot = '${workspaceFolder}',
     }
 }
 
-dap.configurations.typescriptreact = { -- change to typescript if needed
-    {
-        type = 'chrome',
-        request = 'attach',
-        program = '${file}',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        port = 9222,
-        webRoot = '${workspaceFolder}'
-    }
-}
+dap.configurations.javascript = esconfig
+dap.configurations.javascriptreact = esconfig
+dap.configurations.typescript = esconfig
+dap.configurations.typescriptreact = esconfig
 
-require('dap.ext.vscode').load_launchjs()
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<leader>dc', '<cmd>lua require("dap").continue()<CR>', opts) 
+vim.api.nvim_set_keymap('n', '<leader>db', '<cmd>lua require("dap").toggle_breakpoint()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>dl', '<cmd>lua require("dap").step_over()<CR>', opts) 
+vim.api.nvim_set_keymap('n', '<leader>dj', '<cmd>lua require("dap").step_into()<CR>', opts) 
+vim.api.nvim_set_keymap('n', '<leader>dk', '<cmd>lua require("dap").step_out()<CR>', opts) 
+vim.api.nvim_set_keymap('n', '<leader>dbr', '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>dbm', '<cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', opts)
