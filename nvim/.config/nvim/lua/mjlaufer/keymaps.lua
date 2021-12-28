f52052af54c17239761eb858ba2bbe71ce390b38
Matthew Laufer <matthew.j.laufer@gmail.com>
2021-12-27 23:18:52 -0800

    vim: set cursor line; toggle hlsearch

--- nvim/.config/nvim/lua/mjlaufer/keymaps.lua
+++ nvim/.config/nvim/lua/mjlaufer/keymaps.lua
@@ -3,31 +3,30 @@ local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
 -- Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+keymap('', '<Space>', '<Nop>', opts)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Source vimrc
-keymap('n', '<leader><CR>', ':so ~/dotfiles/nvim/.config/nvim/init.lua<CR>',
-       {noremap = true})
+keymap('n', '<leader><CR>', ':so ~/dotfiles/nvim/.config/nvim/init.lua<CR>', {noremap = true})
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap('n', '<S-l>', ':bnext<CR>', opts)
+keymap('n', '<S-h>', ':bprevious<CR>', opts)
 
 -- [Visual] Move selected lines up/down
-keymap('v', 'J', ":m '>+1<CR>gv=gv", opts)
-keymap('v', 'K', ":m '<-2<CR>gv=gv", opts)
+keymap('v', 'J', ':m \'>+1<CR>gv=gv', opts)
+keymap('v', 'K', ':m \'<-2<CR>gv=gv', opts)
 
 -- [Visual] Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
 
 -- [Normal/Visual] yank/delete to clipboard; [Normal] put from clipboard
 keymap('n', '<leader>y', '"*yy<CR>', opts)
--- nvim/.config/nvim/lua/mjlaufer/sets.lua
+++ nvim/.config/nvim/lua/mjlaufer/sets.lua
@@ -6,22 +6,35 @@ local options = {
     expandtab = true, -- Convert tabs to spaces.
     smartindent = true,
     wrap = false,
+    cursorline = true,
     relativenumber = true,
     nu = true, -- Show current line number.
     signcolumn = 'yes', -- Add a left column to display diagnostic signs.
     colorcolumn = '100',
     scrolloff = 8, -- Scroll when cursor is 8 lines from top or bottom.
-    hlsearch = false, -- Remove highlight from search term when search is complete.
+    hlsearch = false,
     incsearch = true, -- Highlight the currently matched string while searching.
     ignorecase = true, -- Ignore casing in search patterns.
     smartcase = true, -- Override ignorecase if search pattern contains uppercase.
     swapfile = false,
     backup = false,
-    errorbells = false
+    errorbells = false,
 }
 
-for k, v in pairs(options) do vim.opt[k] = v end
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
+-- Toggle 'hlsearch'
+vim.cmd [[
+    augroup vimrc-incsearch-highlight
+        autocmd!
+        autocmd CmdlineEnter /,\? :set hlsearch
+        autocmd CmdlineLeave /,\? :set nohlsearch
+    augroup END
+]]
+
+-- Turn on 'wrap' for markdown files
 vim.cmd [[
     augroup MarkdownWrap
         autocmd!
