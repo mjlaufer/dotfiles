9fb31d2104c3843ec4be739735426c7b5dd739e2
Matthew Laufer <matthew.j.laufer@gmail.com>
2022-01-01 14:18:55 -0800

    vim: update lsp keymaps

--- nvim/.config/nvim/lua/mjlaufer/lsp/init.lua
+++ nvim/.config/nvim/lua/mjlaufer/lsp/init.lua
@@ -13,25 +13,36 @@ local on_attach = function(client, bufnr)
 
     local opts = {noremap = true, silent = true}
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- See `:help vim.*` for documentation on any of the below functions
     buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<leader>wl',
-        '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>sa', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    -- Diagnostics
-    buf_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    buf_set_keymap('n', '<leader>ld', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+    require('which-key').register({
+        gD = 'Go to declaration',
+        gd = 'Go to definition',
+        K = 'Hover',
+        gi = 'Show implementations',
+        ['<leader>sa'] = 'Signature help',
+        ['<leader>D'] = 'Type definition',
+        gr = 'List references',
+        ['<leader>rn'] = 'Rename all references',
+        ['<leader>ca'] = 'Code actions',
+        ['<leader>ld'] = 'Show diagnostics',
+        ['[d'] = 'Previous diagnostic',
+        [']d'] = 'Next diagnostic',
+        ['<leader>q'] = 'Add diagnostics to location list',
+    })
 
     -- Prevent tsserver from formatting; use efm instead
     if client.name == 'tsserver' then
