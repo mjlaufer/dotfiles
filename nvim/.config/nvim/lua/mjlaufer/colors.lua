919d3aadc89040d2ab87ca27c0990ec4fe395ab5
Matthew Laufer <matthew.j.laufer@gmail.com>
2022-01-13 08:43:34 -0800

    vim: add lua to colors config

--- nvim/.config/nvim/lua/mjlaufer/colors.lua
+++ nvim/.config/nvim/lua/mjlaufer/colors.lua
@@ -8,7 +8,17 @@ vim.cmd [[
 require('nvim-treesitter.configs').setup {
     highlight = {enable = true},
     incremental_selection = {enable = true},
-    ensure_installed = {'css', 'html', 'javascript', 'json', 'scss', 'tsx', 'typescript', 'yaml'},
+    ensure_installed = {
+        'css',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'scss',
+        'tsx',
+        'typescript',
+        'yaml',
+    },
     context_commentstring = {enable = true, enable_autocmd = false},
 }
 
@@ -17,8 +27,9 @@ parser_config.tsx.used_by = {'javascript', 'typescript.tsx'}
 
 require('colorizer').setup({
     html = {mode = 'foreground'},
-    css = {rgb_fn = true},
+    css = {rgb_fn = true, hsl_fn = true},
     'scss',
     'javascript',
     'typescript',
+    'lua',
 })
