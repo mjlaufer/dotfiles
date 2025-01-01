ad82b007497b6840fe6d5ced3bc1231e00fd5a86
Matthew Laufer <matthew.j.laufer@gmail.com>
2025-01-01 01:46:05 -0800

    vim: replace flash.nvim with vim-sneak

--- nvim/.config/nvim/lua/mjlaufer/plugins/init.lua
+++ nvim/.config/nvim/lua/mjlaufer/plugins/init.lua
@@ -163,54 +163,6 @@ require('lazy').setup({
         enabled = vim.fn.has('nvim-0.10.0') == 1,
     },
     {
-        'folke/flash.nvim',
-        event = 'VeryLazy',
-        ---@type Flash.Config
-        opts = {},
-        keys = {
-            {
-                's',
-                mode = { 'n', 'x', 'o' },
-                function()
-                    require('flash').jump()
-                end,
-                desc = 'Flash',
-            },
-            {
-                'S',
-                mode = { 'n', 'x', 'o' },
-                function()
-                    require('flash').treesitter()
-                end,
-                desc = 'Flash Treesitter',
-            },
-            {
-                'r',
-                mode = 'o',
-                function()
-                    require('flash').remote()
-                end,
-                desc = 'Remote Flash',
-            },
-            {
-                'R',
-                mode = { 'o', 'x' },
-                function()
-                    require('flash').treesitter_search()
-                end,
-                desc = 'Treesitter Search',
-            },
-            {
-                '<c-s>',
-                mode = { 'c' },
-                function()
-                    require('flash').toggle()
-                end,
-                desc = 'Toggle Flash Search',
-            },
-        },
-    },
-    {
         'windwp/nvim-autopairs',
         config = function()
             require('nvim-autopairs').setup({
@@ -227,6 +179,12 @@ require('lazy').setup({
     {
         'windwp/nvim-ts-autotag',
         opts = {},
+    },
+    {
+        'justinmk/vim-sneak',
+        config = function()
+            require('mjlaufer.plugins.sneak')
+        end,
     },
     {
         'iamcco/markdown-preview.nvim',
