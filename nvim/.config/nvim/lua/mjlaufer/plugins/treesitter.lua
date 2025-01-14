2a13c5b0d57c1527dad727cb99ea78b3fa9a83f3
Matthew Laufer <matthew.j.laufer@gmail.com>
2025-01-13 18:25:42 -0800

    vim: add just to treesitter

--- nvim/.config/nvim/lua/mjlaufer/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mjlaufer/plugins/treesitter.lua
@@ -18,6 +18,7 @@ require('nvim-treesitter.configs').setup({
         'javascript',
         'jsdoc',
         'json',
+        'just',
         'lua',
         'markdown',
         'markdown_inline',
