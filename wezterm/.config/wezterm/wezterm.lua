4e09121aa421129923779d71e45aff3d776d46f5
Matthew Laufer <matthew.j.laufer@gmail.com>
2025-06-24 07:32:57 -0700

    wezterm: update config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,65 +2,82 @@ local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "inklight"
+config.color_scheme = 'inklight'
 
 -- Fancy tab bar
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = false
 config.window_frame = {
+    font = wezterm.font({
+        family = 'Roboto',
+        weight = 'Regular',
+    }),
     font_size = 16.0,
     active_titlebar_bg = '#e5e5e5',
     inactive_titlebar_bg = '#e5e5e5',
 }
 config.colors = {
     tab_bar = {
-      background = "#e5e5e5",
-      inactive_tab_edge = "#e5e5e5",
-      active_tab = {
-        bg_color = "#eeeeee",
-        fg_color = "#353535",
-        intensity = "Bold",
-      },
-      inactive_tab = {
-        bg_color = "#e5e5e5",
-        fg_color = "#555555",
-        intensity = "Normal",
-      },
-      inactive_tab_hover = {
-        bg_color = "#eeeeee",
-        fg_color = "#353535",
-      },
-      new_tab = {
-        bg_color = "#e5e5e5",
-        fg_color = "#555555",
-      },
-      new_tab_hover = {
-        bg_color = "#eeeeee",
-        fg_color = "#353535",
-      },
+        background = '#e5e5e5',
+        inactive_tab_edge = '#e5e5e5',
+        active_tab = {
+            bg_color = '#eeeeee',
+            fg_color = '#353535',
+            intensity = 'Normal',
+        },
+        inactive_tab = {
+            bg_color = '#e5e5e5',
+            fg_color = '#555555',
+            intensity = 'Normal',
+        },
+        inactive_tab_hover = {
+            bg_color = '#eeeeee',
+            fg_color = '#353535',
+        },
+        new_tab = {
+            bg_color = '#e5e5e5',
+            fg_color = '#555555',
+        },
+        new_tab_hover = {
+            bg_color = '#eeeeee',
+            fg_color = '#353535',
+        },
     },
 }
 
+-- Combine Mac OS window controls with WezTerm's tab bar.
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 
-config.font = wezterm.font_with_fallback({
-    { family = 'JetBrains Mono', weight = 'Regular' },
-    { family = 'Symbols Nerd Font' },
-})
-config.font_size = 16.0
-
+-- Window settings
 config.initial_cols = 100
 config.initial_rows = 40
-
-config.scrollback_lines = 2000
-
 config.window_background_opacity = 1.0
 config.window_padding = {
     left = 16,
-    right = 16,
+    right = 12,
     top = 12,
     bottom = 12,
 }
+
+-- Font settings
+config.font = wezterm.font({
+    family = 'JetBrains Mono',
+    weight = 'Medium',
+})
+config.font_rules = {
+    {
+        intensity = 'Bold',
+        font = wezterm.font({
+            family = 'JetBrains Mono',
+            weight = 'ExtraBold',
+        }),
+    },
+}
+config.font_size = 16.0
+config.line_height = 1.1
+config.freetype_load_target = 'Light'
+config.freetype_render_target = 'HorizontalLcd'
+config.front_end = 'WebGpu'
 
 return config
